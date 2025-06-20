002deadde19de2d8756fd5ed6da3bc7c17b6022a
ferreira0717 <ferreira0717@gmail.com>
2025-06-19 23:27:17 -0500

    creando routing en express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hola mundo desde DevTree 19 junio')
+const express = require('express');
+
+const app = express()
+
+//rounting
+app.get('/', (req, res) => {
+    res.send('Hola mundo en Express')
+})
+app.get('/ecommerce', (req, res) => {
+    res.send('Hola este es el ecommerce')
+})
+app.get('/nosotros', (req, res) => {
+    res.send('Nosotros somos nosotros')
+})
+
+app.listen(4000, () => {
+    console.log('servidor funcionando');
+})
